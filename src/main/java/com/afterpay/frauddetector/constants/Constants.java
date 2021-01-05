882f7d85d429457508a6b47393ba52d3d758eb43
NAGARRO\arjunduggal <arjun.duggal@nagarro.com>
2021-01-05 17:20:33 +1100

    Adding interface implementations and logback settings.

---
+++ src/main/java/com/afterpay/frauddetector/constants/Constants.java
@@ -0,0 +1,42 @@
+package com.afterpay.frauddetector.constants;
+
+/**
+ * This class keeps the static list of all the constants being used in the application.
+ *
+ * @author arjunduggal
+ */
+public class Constants {
+  public static final String LOCAL_SPARK_MASTER = "local[*]";
+  
+  public static final String APP_NAME = "FraudDetector";
+  
+  public static final long ZERO = 0;
+
+  public static final String YYYY_MM_DD_T_HH_MM_SS = "yyyy-MM-dd'T'HH:mm:ss";
+
+  public static final String INVALID_AMOUNT = "Invalid amount found for the transaction record.{0}";
+
+  public static final String INVALID_DATE = "Invalid date found for the transaction record.\n{0}";
+
+  public static final String INVALID_TRANSACTION_RECORD = "Invalid transaction record found in the file.\n{0}";
+
+  public static final String CACHE_DATA_IS_NOT_IN_SYNC =
+      "The cache data is not in sync for credit card {0}. Please check the debug logs for further details.";
+  
+  public static final String INVALID_THRESHOLD_AMOUNT =
+      "The threshold amount provided is invalid. Please try again wih a valid value.";
+  
+  public static final String VALID_EXTENSIONS = "csv";
+  
+  public static final String INVALID_FILENAME =
+      "Either filename is invalid or file does not exist at specified path. Please verify and pass a valid filename for a csv file.";
+
+  public static final String INVALID_ARGUMENTS =
+      "Please pass a valid filename and threshold value as below. \n"
+          + "mvn -q spring-boot:run -Dspring-boot.run.arguments=\"--threshold=<threshold-value> --filename=<filename-value>\" \n\n"
+          + "In case you are running it via JAR file, use: \njava -jar <path>/fraud-detector-1.0.0-SNAPSHOT.jar  --threshold=<threshold-value> --filename=<filename-value>";
+
+  private Constants() {
+    // Added to avoid object instantiation
+  }
+}
